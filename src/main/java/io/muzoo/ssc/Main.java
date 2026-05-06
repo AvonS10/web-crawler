281f10ebab888d3ea2c616d7b8a8ac407d9c7b05
Pasin Visuttipinate <pasin.visuttipinate@gmail.com>
2026-05-06 15:11:03 +0700

    setting up tracker

---
+++ src/main/java/io/muzoo/ssc/Main.java
@@ -0,0 +1,9 @@
+package io.muzoo.ssc;
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
+
+public class Main extends SscAssignment {
+
+    public static void main(String[] args) {
+        //...
+    }
+}
